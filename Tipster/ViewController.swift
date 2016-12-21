83e8d1b10d048b92fc7e05a76d3dd2a69342569d
Anubhav Garg <anubhavgarg@Anubhavs-MacBook-Air.local>
2016-12-20 19:57:11 -0600

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Anubhav Garg on 12/20/16.
+//  Copyright © 2016 Anubhav Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
